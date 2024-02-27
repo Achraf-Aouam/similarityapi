06ef5656f58efc8cb1dee7b6f491ef7ff994555e
Achraf-Aouam <a.aouam@aui.ma>
2024-02-27 22:34:58 +0000

    v2

--- src/components/ApiDashboard.tsx
+++ src/components/ApiDashboard.tsx
@@ -1,9 +1,60 @@
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
 import { FC } from "react";
+import { formatDistance } from "date-fns";
+import LargeHeading from "@/ui/LargeHeading";
+import Paragraph from "./ui/Paragraph";
+import { Input } from "./ui/Input";
+import Table from "./Table";
+import ApiKeyOptions from "./ApiKeyOptions";
 
-interface ApiDashboardProps {}
+const ApiDashboard = async () => {
+  const user = await getServerSession(authOptions);
+  if (!user) {
+    return notFound();
+  }
 
-const ApiDashboard: FC<ApiDashboardProps> = ({}) => {
-  return <div>ApiDashboard</div>;
+  const apiKeys = await db.apiKey.findMany({
+    where: { userId: user.user.id },
+  });
+
+  const activeApikey = apiKeys.find((apikey) => apikey.enabled);
+
+  if (!activeApikey) {
+    notFound();
+  }
+
+  const userRequests = await db.apiRequest.findMany({
+    where: {
+      apiKeyId: {
+        in: apiKeys.map((apikey) => apikey.id),
+      },
+    },
+  });
+
+  const serializableRequests = userRequests.map((req) => ({
+    ...req,
+    timestamp: formatDistance(new Date(req.timestamp), new Date()),
+  }));
+
+  return (
+    <div className="container flex flex-col gap-6 ">
+      <LargeHeading>Welcome Back, {user.user.name}</LargeHeading>
+      <div className=" flex flex-col md:flex-row gap-4 justify-center md:justify-start items-center">
+        <Paragraph>Your API key:</Paragraph>
+        <Input className="w-fit truncate" readOnly value={activeApikey.key} />
+        <ApiKeyOptions
+          apiKeyId={activeApikey.id}
+          apiKeyKey={activeApikey.key}
+        />
+      </div>
+      <Paragraph className="text-center md:text-left mt-4 -mb-4">
+        Your API history:
+      </Paragraph>
+      <Table userRequests={serializableRequests} />
+    </div>
+  );
 };
-
 export default ApiDashboard;
